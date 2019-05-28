79b1c61443657b2481016cdb8dddca4824f15f1c
John <jpena91@gmail.com>
2019-05-28 09:13:05 -0400

    add form input to backend

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -3,4 +3,7 @@ class ShoppingCartsController < ApplicationController
   def index
     @shoppingCart = ShoppingCart.all
     render json: @shoppingCart
+  end
+
+  
 end
--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -1,8 +1,30 @@
 class SneakersController < ApplicationController
 
   def index
-    @sneakers= Sneaker.all  
+    @sneakers= Sneaker.all
     render json: @sneakers
+  end
+
+  def show
+
+  end
+
+  def new
+    @sneaker = Sneaker.new(params)
+  end
+
+  def create
+    @sneaker = Sneaker.create(sneaker_params)
+
+    render json: @sneakers
+
+  end
+
+
+  private
+
+  def sneaker_params
+    params.require(:sneaker).permit(:name, :brand, :price, :description, :colorway, :image_url)
   end
 
 
