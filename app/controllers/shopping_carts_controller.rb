faeff4c0a438077a566ad43e5fe3655f89010904
John <jpena91@gmail.com>
2019-05-30 00:33:06 -0400

    add confirmation, remove from cart, fix styling

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -5,5 +5,16 @@ class ShoppingCartsController < ApplicationController
     render json: @shoppingCart
   end
 
-  
+
+  def new
+    shoppingcart = ShoppingCart.new
+  end
+
+
+  def create
+    shoppingcart = ShoppingCart.create(:sneaker_id)
+  end
+
+
+
 end
